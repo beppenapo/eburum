eeb9ddd2ba782e79a95033072ca2682ed8b704a7
Giuseppe Naponiello <beppenapo@arc-team.com>
2017-06-05 19:51:42 +0200

    recode

--- inc/db.php
+++
@@ -1,9 +0,0 @@
-<?php
-$dbhost = 'localhost';
-$dbusername = 'beppe';
-$password='alfaomega';
-$database_name = 'eburum';
-$schema = 'eburum';
-$connection = pg_connect("host=$dbhost user=$dbusername password=$password dbname=$database_name")
-or die ("Impossibile connettersi al server \n" . pg_last_error($connection));
-?>
--- log.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-require_once("inc/db.php");
-if($_GET['t']=='out'){
-  session_destroy();
-  header("Location:index.php");
-}
-?>
